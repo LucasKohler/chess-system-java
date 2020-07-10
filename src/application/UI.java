7a04ef77a61f4b51e013865635c7a49e864c999b
LucasKohler <lucaskohlermarqueswork@gmail.com>
2020-07-10 16:43:14 -0300

    Checkmate login

--- src/application/UI.java
+++ src/application/UI.java
@@ -56,9 +56,14 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
+		if (!chessMatch.getCheckMate()) {
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		if(chessMatch.getCheack()) {
-			System.out.println("CHECK!");
+			if(chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		}else{
+			System.out.println("CHEACKMATE!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
 	}
 
