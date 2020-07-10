62ff1fdd9b34f09764abaa0ea9f6d784fd08b0ac
LucasKohler <lucaskohlermarqueswork@gmail.com>
2020-07-10 12:07:41 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -57,6 +57,9 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		if(chessMatch.getCheack()) {
+			System.out.println("CHECK!");
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
@@ -103,11 +106,11 @@ public class UI {
 		System.out.println("Captured pieces");
 		System.out.print("White: ");
 		System.out.print(ANSI_WHITE);
-		System.out.println(Arrays.deepToString(white.toArray()));
+		System.out.println(Arrays.toString(white.toArray()));
 		System.out.print(ANSI_RESET);
 		System.out.print("black: ");
 		System.out.print(ANSI_YELLOW);
-		System.out.println(Arrays.deepToString(black.toArray()));
+		System.out.println(Arrays.toString(black.toArray()));
 		System.out.print(ANSI_RESET);
 	}
 }
